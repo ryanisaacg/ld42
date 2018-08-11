ad8f4df58796cbabf9801a889f51582d4c4c4117
Ryan Goldstein <ryanisaacg@gmail.com>
2018-08-11 12:24:28 -0400

    Add jumping functionality

--- src/input.rs
+++ src/input.rs
@@ -11,4 +11,29 @@ pub fn system(window: &Window, store: &mut Store) {
         accel.x -= WALK_ACCEL;
     }
     store.accel[store.player] = Some(accel);
+    if window.keyboard()[Key::Space] == ButtonState::Pressed {
+        //TODO: only allow jumps on the ground
+        let iso = vec_to_iso(store.bounds[store.walls].position);
+        let ray = Ray::new(
+            store.bounds[store.player].position.into(),
+            na::Vector2::new(0.0, 0.02),
+        );
+        let touch = contact(
+            &vec_to_iso(store.bounds[store.player].position),
+            store.bounds[store.player].shape.as_ref(),
+            &vec_to_iso(store.bounds[store.walls].position),
+            store.bounds[store.walls].shape.as_ref(),
+            0.001
+        );
+        match touch {
+            Some(touch) => {
+                if touch.normal.y > 0.0 {
+                    let speed = store.speed[store.player].unwrap();
+                    store.speed[store.player] = Some(Vector::new(speed.x, -0.12));
+             
+                }
+            }
+            None => ()
+        }
+    }
 }
