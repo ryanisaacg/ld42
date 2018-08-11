d9a67255eaa93d695e9ce9c53f111cfc93a996ea
Ryan Goldstein <ryanisaacg@gmail.com>
2018-08-11 13:48:30 -0400

    Add an idle cycle to the player

--- src/assets.rs
+++ src/assets.rs
@@ -1,10 +1,32 @@
 use super::*;
 
-pub const IMAGES: &[&str] = &["astronaut.png"];
+pub const IMAGES: &[&str] = &[
+    "astronaut.png",
+    "astronaut_idle1.png",
+    "astronaut_idle2.png",
+    "astronaut_idle3.png",
+    "astronaut_idle4.png",
+];
 pub const SOUNDS: &[&str] = &[];
 
 pub struct Assets {
     pub player: Image,
+    pub player_idle: Animation,
+}
+
+// TODO: include this in quicksilver
+fn from_sheet(image: Image, columns: u32, frame_delay: u32) -> Animation {
+    let mut subimage_size = image.area().size();
+    subimage_size.x = subimage_size.x / (columns as f32);
+    println!("{:?}", subimage_size);
+    let mut frames = Vec::new();
+    for i in 0..columns {
+        let view = Rectangle::new(Vector::X * subimage_size.x * i, subimage_size);
+        println!("{:?}", view);
+        frames.push(image.subimage(view));
+    }
+    println!("{:?}", frames);
+    Animation::new(frames, frame_delay)
 }
 
 impl Assets {
@@ -30,6 +52,12 @@ impl Assets {
         let loaders = image_loader.join(sound_loader);
         Asset::new(loaders.map(|(images, sounds)| Assets {
             player: images["astronaut.png"].clone(),
+            player_idle: Animation::new(vec![
+                images["astronaut_idle1.png"].clone(),
+                images["astronaut_idle2.png"].clone(),
+                images["astronaut_idle3.png"].clone(),
+                images["astronaut_idle4.png"].clone(),
+            ], 11)
         }))
     }
 }
--- src/draw.rs
+++ src/draw.rs
@@ -1,14 +1,19 @@
 use super::*;
 use quicksilver::geom::Shape;
 
-pub fn system(window: &mut Window, assets: &Assets, store: &mut Store) -> Result<()> {
+// TODO: Background with an animation
+// TODO: are subimages broken??
+
+pub fn system(window: &mut Window, assets: &mut Assets, store: &mut Store) -> Result<()> {
     window.clear(Color::WHITE)?;
     let scale = Transform::scale((100, 100));
     {
         let position = scale * store.bounds[store.player].position;
         let aabb = store.bounds[store.player].shape.aabb(&na::one());
         let rect: Rectangle = aabb.into();
-        window.draw_ex(&rect.translate(position), Background::Img(&assets.player), scale, 0);
+        assets.player_idle.tick();
+        let frame = assets.player_idle.current_frame();
+        window.draw_ex(&rect.translate(position), Background::Img(&frame), scale, 0);
     }
     /*{
         let bounds = store.bounds[store.walls].clone();
