a11506c6ac99e4312a386d1a6f728dd9a619bc1b
Ryan Goldstein <ryanisaacg@gmail.com>
2018-08-11 20:07:14 -0400

    Implement the bombs detonating

--- src/bomb.rs
+++ src/bomb.rs
@@ -6,6 +6,20 @@ pub fn system(store: &mut Store) {
         if let Some(timer) = &mut store.detonate_timer[entity] {
             *timer -= 1;
             if *timer == 0 {
+                let center = store.bounds[entity].position;
+                let rect = Rectangle::new_sized((0.64, 0.64)).with_center(center);
+                let mut shapes = vec![
+                    (vec_to_iso(center), ShapeHandle::new(rect_to_cuboid(rect)))
+                ];
+                {
+                    let compound = store.bounds[store.walls].shape
+                        .as_shape::<Compound<f32>>().unwrap();
+                    shapes.extend(compound.shapes().iter().cloned());
+                }
+                store.bounds[store.walls] = Bounds {
+                    position: Vector::ZERO,
+                    shape: ShapeHandle::new(Compound::new(shapes))
+                };
                 store.deleted.push(idx);
             }
         }
